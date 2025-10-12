a25fde85bb55b6242c4ba50058d08a79e21e3171
hasabob <graemejgordon@gmail.com>
2025-10-13 07:02:35 +1000

    Update Main.cpp

--- CollatzConjecture/Main.cpp
+++ CollatzConjecture/Main.cpp
@@ -1,8 +1,57 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+unsigned long long x_orig;
+unsigned long long x_old;
+unsigned long long x;
+
+int saveFile(const char* filename, const unsigned long long data)
+{
+	// Setup
+	ofstream file;
+
+	// Save File
+	file.open(filename);
+	file << data;
+	file.close();
+	return 0;
+}
+
+unsigned long long readFile(const char* filename)
+{
+	string line;
+	ifstream myfile(filename);
+	unsigned long long value = 0;
+
+	if (myfile)
+	{
+		myfile >> value;
+		myfile.close();
+	}
+	else
+	{
+		cout << "Unable to open file";
+	}
+
+	return value;
+}
 
 int main()
 {
-	unsigned long long x_orig = 2;								/*295000000000000000000000*/
+	const char* SAVE_FILE = "collatz.log";
+	unsigned long long savedValue = readFile(SAVE_FILE);
+	cout << "Reading " << readFile(SAVE_FILE) << " from " << SAVE_FILE << "\n";
+	cout << savedValue << "\n";
+	if (savedValue != 0)
+	{
+		x_orig = readFile(SAVE_FILE);
+	}
+	else
+	{
+		x_orig = 3;							/*295000000000000000000000*/
+	}
 	if (x_orig % 2 == 0)					// Prevents Collatz sequence from being even
 	{
 		x_orig++;
@@ -15,6 +64,7 @@ int main()
 		if (i % 100000 == 0)
 		{
 			std::cout << x << "\n";
+			saveFile(SAVE_FILE, x_old);
 		}
 		while (x > x_old - 1)				// Stops x value dropping below tested numbers
 		{
